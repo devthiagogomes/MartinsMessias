c885a50f44ed8e5c8d705050b18d7a33ea8a11d5
Thiago Gomes <thiago.gomes@splice.com.br>
2025-06-17 09:12:36 -0300

    changed nav-link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../css/Header.css';
 import SubLogo from '../assets/sublogo.png';
 import Logo from '../assets/logo.png';
@@ -6,17 +6,27 @@ import Logo from '../assets/logo.png';
 export default function Header() {
   return (
     <nav>
-      <Link to="/" className="logo-container">
+      <NavLink to="/" className="logo-container">
         <img src={Logo} alt="Logo" style={{ height: '70px' }} />
         <img src={SubLogo} alt="SubLogo" style={{ height: '100px' }} />
-      </Link>
+      </NavLink>
 
       <div className="button-container">
-        <Link to="/" className="nav-link">Início</Link>
-        <Link to="/nossos-valores" className="nav-link">Nossos Valores</Link>
-        <Link to="/investimento" className="nav-link">Investimento</Link>
-        <Link to="/plano-de-acao" className="nav-link">Plano de Ação</Link>
-        <Link to="/contato" className="nav-link">Contato</Link>
+        <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+          Início
+        </NavLink>
+        <NavLink to="/nossos-valores" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+          Nossos Valores
+        </NavLink>
+        <NavLink to="/investimento" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+          Investimento
+        </NavLink>
+        <NavLink to="/plano-de-acao" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+          Plano de Ação
+        </NavLink>
+        <NavLink to="/contato" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+          Contato
+        </NavLink>
       </div>
     </nav>
   );
